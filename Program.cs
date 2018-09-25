0c9ec777999a460d9d96d23c814e1dd9c3d5cc1f
Isaiah White <isaiahwhite208@gmail.com>
2018-09-25 09:29:08 -0600

    push

--- Program.cs
+++ Program.cs
@@ -2,11 +2,11 @@
 
 namespace csharp_login
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+ class Program
+ {
+  static void Main(string[] args)
+  {
+
+  }
+ }
 }
